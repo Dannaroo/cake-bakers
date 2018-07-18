009d36b8309e0f601907ebdbc60f5b85c2506901
Danny Ward <liege88@gmail.com>
2018-07-18 21:50:45 +0800

    add ourcakes.html functionality to sort the price of cake divs in ascending and descending order. Adds dollar signs in JS rather than explicitly in HTML.

--- js/app.js
+++ js/app.js
@@ -12,7 +12,6 @@ $('#alphBnt').on('click', function () {
 });
 
 // Sort ourcake Divs Z-A
-
 $('#reverseAlphBnt').on('click', function () {
  var alphabeticallyOrderedDivs = $('.col-sm-6').sort(function(a, b) {
        return String.prototype.localeCompare.call($(b).text().toLowerCase(), $(a).text().toLowerCase());
@@ -24,42 +23,42 @@ $('#reverseAlphBnt').on('click', function () {
 
 });
 
-const prices = document.querySelectorAll('.card-body .btn-secondary');
-const prices1 = prices.innerHTML;
-const array1 = [];
-
-for(let i = 0; i < prices.length; i += 1) {
-  eachPrice = prices[i].innerHTML;
-  array1.push(eachPrice);
-  console.log(i);
-  console.log(eachPrice);
-}
-console.log(array1);
-array1.sort();
-console.log(array1);
-
-prices[1].sort(function(a, b) {
-  var nameA = a.innerHTML.toUpperCase(); // ignore upper and lowercase
-  var nameB = b.innerHTML.toUpperCase(); // ignore upper and lowercase
-  if (nameA < nameB) {
-    return -1;
-  }
-  if (nameA > nameB) {
-    return 1;
-  }
-
-  // names must be equal
-  return 0;
+//Sort ourcake Divs price low - high
+$('#priceAscend').on('click', function () {
+  $('.card-deck').each(function(){
+    $(this).html(
+      $(this).find('.col-sm-6').sort(function(a, b) {
+        let dsa = parseInt($(a).find('.btn-secondary').eq(0).text()),
+            dsb = parseInt($(b).find('.btn-secondary').eq(0).text());
+        return (dsa > dsb ? 1 : (dsa < dsb) ? -1 : 0);
+      })
+    );
+  });
 });
-console.log(prices);
-// Sort ourcake Divs by price
-// $('#priceAscend').on('click', function () {
-//  var alphabeticallyOrderedDivs = $('.col-sm-6 .btn-secondary').sort(function(a, b) {
-//        return String.prototype.localeCompare.call($(a).text().toLowerCase(), $(b).text().toLowerCase());
-//      });
-//
-//  var cardDeck = $(".card-deck");
-//  cardDeck.detach().empty().append(alphabeticallyOrderedDivs);
-//  $('#bin').append(cardDeck);
-//
-// });
+
+//Sort ourcake Divs price high - low
+$('#priceDescend').on('click', function () {
+  $('.card-deck').each(function(){
+    $(this).html(
+      $(this).find('.col-sm-6').sort(function(a, b) {
+        let dsa = parseInt($(a).find('.btn-secondary').eq(0).text()),
+            dsb = parseInt($(b).find('.btn-secondary').eq(0).text());
+        return (dsa > dsb ? -1 : (dsa < dsb) ? 1 : 0);
+      })
+    );
+  });
+});
+
+//prepends dollar sign to price tags in ourcakes
+const cardDeck = document.querySelector('.card-deck');
+const priceTag = cardDeck.querySelectorAll('a.btn-secondary');
+function attachDollarSign(a) {
+  let dollarSign = document.createElement('span');
+  dollarSign.innerHTML = "$";
+  a.insertBefore(dollarSign, a.firstChild);
+}
+
+//Call the function for all price tags
+for (let i = 0; i < priceTag.length; i += 1 ) {
+  attachDollarSign(priceTag[i]);
+}
