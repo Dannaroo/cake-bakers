b9c0353bbf0d84be39ca938896da7223e85708d9
Danny Ward <liege88@gmail.com>
2018-07-21 22:20:07 +0800

    seamlessly add/remove '$' to prices so that JS code sortation works as expected. added functionality to search bar. now able to search for partial strings in our cakes. only works on ourcakes.html. added button in ourcakes to clear the search results.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+const cardDeck = document.querySelector('.card-deck');
+const priceTag = cardDeck.querySelectorAll('a.btn-secondary');
+const search = document.getElementById('searchTerm');
+const submitButton = document.querySelector('#submitSearch');
+const list = document.querySelectorAll('#bin h5');
+const clearButton = document.querySelector('#clear');
+
+
+//Remove dollar sign for sorting purposes
+function removeDollarSign(a) {
+  let removeDollarSign = a.querySelector("span");
+  if (removeDollarSign) {
+    a.removeChild(removeDollarSign);
+  }
+}
+
+//prepends dollar sign to price tags in ourcakes
+function attachDollarSign(a) {
+  let dollarSign = document.createElement('span');
+  dollarSign.innerHTML = "$";
+  a.insertBefore(dollarSign, a.firstChild);
+}
+//Call the function for all price tags
+for (let i = 0; i < priceTag.length; i += 1 ) {
+  attachDollarSign(priceTag[i]);
+}
 
 // Sort ourcake Divs A-Z
 $('#alphBnt').on('click', function () {
@@ -26,6 +52,11 @@ $('#reverseAlphBnt').on('click', function () {
 //Sort ourcake Divs price low - high
 $('#priceAscend').on('click', function () {
   $('.card-deck').each(function(){
+    //remove dollar sign for sorting purposes
+    for (let i = 0; i < priceTag.length; i += 1 ) {
+      removeDollarSign(priceTag[i]);
+    }
+    //sort
     $(this).html(
       $(this).find('.col-sm-6').sort(function(a, b) {
         let dsa = parseInt($(a).find('.btn-secondary').eq(0).text()),
@@ -39,6 +70,11 @@ $('#priceAscend').on('click', function () {
 //Sort ourcake Divs price high - low
 $('#priceDescend').on('click', function () {
   $('.card-deck').each(function(){
+    //remove dollar sign for sorting purposes
+    for (let i = 0; i < priceTag.length; i += 1 ) {
+      removeDollarSign(priceTag[i]);
+    }
+    //sort
     $(this).html(
       $(this).find('.col-sm-6').sort(function(a, b) {
         let dsa = parseInt($(a).find('.btn-secondary').eq(0).text()),
@@ -49,16 +85,24 @@ $('#priceDescend').on('click', function () {
   });
 });
 
-//prepends dollar sign to price tags in ourcakes
-const cardDeck = document.querySelector('.card-deck');
-const priceTag = cardDeck.querySelectorAll('a.btn-secondary');
-function attachDollarSign(a) {
-  let dollarSign = document.createElement('span');
-  dollarSign.innerHTML = "$";
-  a.insertBefore(dollarSign, a.firstChild);
-}
+//Operate search bar function
+submitButton.addEventListener('click', () => {
+  for (i = 0; i < list.length; i += 1) {
+    let title = list[i].innerHTML;
+    if ( title.toLowerCase().includes(search.value.toLowerCase()) ) {
+      list[i].parentNode.parentNode.parentNode.style.display="block";
+    } else {
+      list[i].parentNode.parentNode.parentNode.style.display="none";
+    }
+  }
+  search.value = "";
+});
 
-//Call the function for all price tags
-for (let i = 0; i < priceTag.length; i += 1 ) {
-  attachDollarSign(priceTag[i]);
-}
+//Operate clear search button
+clearButton.addEventListener('click', () => {
+  for (i = 0; i < list.length; i += 1) {
+    if (list[i].parentNode.style.display="none") {
+      list[i].parentNode.style.display="block";
+    }
+  }
+});
